eda5d341f87bcd66b47ccded24b3771c41467338
Anik <mdanikhasannaim@gmail.com>
2026-08-19 01:06:48 +0600

    d not sloved

---
+++ CC-STARTERS 228 (Div-4)/Min_And_Operations.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int ans = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 0)
+                ans += i;
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
